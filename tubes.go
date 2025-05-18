169b3820ce6d8f43745daa66950b9e7029383e3f
Donzelll <arthurpaliling990@gmail.com>
2025-05-18 23:24:10 +0700

    time

--- tubes.go
+++ tubes.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	if t.Hour() < 12 {
+		fmt.Println("Good morning!")
+	} else if t.Hour() < 17 {
+		fmt.Println("Good afternoon!")
+	} else {
+		fmt.Println("Good evening!")
+	}
+}
